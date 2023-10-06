4581292a3990527f08fb3d0f57374336a6d34dcf
Raub Camaioni <raub.j.camioni.civ@army.mil>
2023-10-05 22:52:17 -0400

    cleanup main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import svg_peg
 import svg_symbols
 import numpy as np
+from matplotlib import pyplot as plt
 
-if __name__ == "__main__":
-    import argparse
-    from matplotlib import pyplot as plt
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", type=str)
-    args = parser.parse_args()
-
-    with open(args.i, "r") as f:
-        svg_string = f.read()
+def display_svg_path(svg_string: str):
+    """display svg_paths with matplotlib"""
 
     tree = svg_peg.parse(svg_string)
     tokens = svg_symbols.tree_to_tokens(tree)
@@ -20,7 +14,7 @@ if __name__ == "__main__":
         print(p)
 
     for path in svg_symbols.tokens_to_path(tokens, resolution=20):
-        # plt.plot(path[:, 0], path[:, 1])
+        plt.plot(path[:, 0], path[:, 1])
         plt.scatter(
             path[:, 0],
             path[:, 1],
@@ -32,3 +26,16 @@ if __name__ == "__main__":
         )
 
     plt.show()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", type=str)
+    args = parser.parse_args()
+
+    with open(args.i, "r") as f:
+        svg_string = f.read()
+
+    display_svg_path(svg_string)
